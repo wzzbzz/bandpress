48ec61e8f3f95ca47f59cbdda9f78ecd64de6b07
James P Williams <James@Macs-MacBook-Air-2.local>
2021-11-17 10:09:19 -0500

    modify default audio / video rendering

--- bandpress/Views/ComponentViews/AudioView.php
+++ bandpress/Views/ComponentViews/AudioView.php
@@ -9,11 +9,10 @@ class AudioView{
     }
     public function render(){
         ?>
-        <div class="mb-md-5 mt-md-4 pb-5 d-flex justify-content-center"><?=$this->data->title();?></div>
-        <div class="mb-md-5 mt-md-4 pb-5 d-flex justify-content-center">
+        <div class="mb-2 d-flex justify-content-center">
         <audio controls>
-        <source src="<?=$this->data->resource_url();?>" type="<?=$this->data->mime_type();?>">
-        Your browser does not support the audio element.
+            <source src="<?=$this->data->resource_url();?>" type="<?=$this->data->mime_type();?>">
+            Your browser does not support the audio element.
         </audio>
         </div>
         <?php
--- bandpress/Views/ComponentViews/VideoView.php
+++ bandpress/Views/ComponentViews/VideoView.php
@@ -9,9 +9,8 @@ class VideoView{
     }
     public function render(){
         ?>
-        <div class="mb-md-5 mt-md-4 pb-5 d-flex justify-content-center"><?=$this->data->title();?></div>
-        <div class="container mb-md-5 mt-md-4 pb-5 d-flex justify-content-center">
-        <video controls width="50%">
+        <div class="container">
+        <video controls width="100%">
         <source src="<?=$this->data->resource_url();?>" type="<?=$this->data->mime_type();?>">
         Your browser does not support the audio element.
         </video>
