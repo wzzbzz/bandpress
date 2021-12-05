740a2b1015c2cc94cfa3a90ec41bdb981f7b0b75
James P Williams <James@Macs-MacBook-Air-2.local>
2021-12-05 11:25:02 -0500

    getUserByLogin replaces getUserByDisplayName

--- vinepress/Models/Users.php
+++ vinepress/Models/Users.php
@@ -4,8 +4,8 @@ namespace vinepress\Models;
 
 class Users extends Model{
 
-    public function getUserByDisplayName($display_name){
-        $sql = "SELECT ID from wp_users WHERE display_name='{$display_name}' LIMIT 1";
+    public function getUserByUserLogin($user_login){
+        $sql = "SELECT ID from wp_users WHERE user_login='{$user_login}' LIMIT 1";
         if(!empty($results = $this->get_results($sql))){
             $wpuser = new \WP_User($results[0]);
             return new User($wpuser);
