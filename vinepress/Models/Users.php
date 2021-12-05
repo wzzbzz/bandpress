1ebfe98c032cc6db0130d1c164c3d48fbd764573
James P Williams <James@Macs-MacBook-Air-2.local>
2021-12-05 11:26:16 -0500

    replace query with wp api

--- vinepress/Models/Users.php
+++ vinepress/Models/Users.php
@@ -5,13 +5,6 @@ namespace vinepress\Models;
 class Users extends Model{
 
     public function getUserByUserLogin($user_login){
-        $sql = "SELECT ID from wp_users WHERE user_login='{$user_login}' LIMIT 1";
-        if(!empty($results = $this->get_results($sql))){
-            $wpuser = new \WP_User($results[0]);
-            return new User($wpuser);
-        }
-
-        else return false;
-
+        return get_user_by('login',$user_login);
     }
 }
