94aa5d8dbbbd32756f5f9bf8dc508ae94d83d770
James P Williams <James@Macs-MacBook-Air-2.local>
2021-12-05 12:30:36 -0500

    fix getUser

--- vinepress/Models/Users.php
+++ vinepress/Models/Users.php
@@ -5,6 +5,13 @@ namespace vinepress\Models;
 class Users extends Model{
 
     public function getUserByUserLogin($user_login){
-        return get_user_by('login',$user_login);
+
+        $user =  get_user_by('login',$user_login);
+        if(!is_wp_error($user)){
+            return new User($user);
+        }
+        else{
+            return $user;
+        }
     }
 }
